3bb1a729635cbb9a6e2d5e9b0860d6213c9f15e1
omniamohamed16 <omnia.mohamedd.40@gmail.com>
2025-03-08 01:09:29 +0200

    editing setting box

--- main.js
+++ main.js
@@ -1,24 +1,50 @@
+// Usable functions
+function removeActiveClass(elements) {
+  elements.forEach((el) => {
+    el.classList.remove("active");
+  });
+}
+
 // Dark Mode
-themeBtn = document.querySelector(".theme");
+themeBtns = document.querySelectorAll(".theme-div span");
+themeLight = document.querySelector(".light");
+themeDark = document.querySelector(".dark");
 body = document.body;
 ModeLS = localStorage.getItem("Mode");
 body.setAttribute("data-bs-theme", ModeLS);
-themeBtn.addEventListener("click", function () {
+if (ModeLS === "dark") {
+  themeDark.classList.add("active");
+} else if (ModeLS === "light") {
+  themeLight.classList.add("active");
+}
+
+
+themeLight.addEventListener("click", function () {
+  removeActiveClass(themeBtns);
+  this.classList.add("active");
   if (body.getAttribute("data-bs-theme") === "dark") {
     body.setAttribute("data-bs-theme", "light");
     localStorage.setItem("Mode", "light");
-  } else {
+  }
+});
+
+
+themeDark.addEventListener("click", function () {
+  removeActiveClass(themeBtns);
+  this.classList.add("active");
+  if (body.getAttribute("data-bs-theme") === "light") {
     body.setAttribute("data-bs-theme", "dark");
     localStorage.setItem("Mode", "dark");
   }
-  console.log(ModeLS);
 });
 
 // setting gear
 settingGear = document.querySelector(".setting");
 settingList = document.querySelector(".setting-list");
 
-settingGear.addEventListener("click", () => {
-  document.querySelector(".fa-gear").classList.toggle("fa-spin");
-  settingList.classList.toggle("hidden")
+settingGear.addEventListener("click", (event) => {
+  if (event.target.classList.contains("fa-gear")) {
+    document.querySelector(".fa-gear").classList.toggle("fa-spin");
+    settingList.classList.toggle("hidden");
+  }
 });
